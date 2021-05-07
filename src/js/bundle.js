37c66f8612354f54a77e89adb6c4d04804688bac
Amarvin <anthony@glacial.com>
2021-05-06 20:33:15 -0400

    initial setup for skills test!

---
+++ src/js/bundle.js
@@ -0,0 +1,8 @@
+import bootstrap from 'bootstrap';
+
+var dropdownElementList = [].slice.call(
+	document.querySelectorAll('.dropdown-toggle')
+);
+var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+	return new bootstrap.Dropdown(dropdownToggleEl);
+});
